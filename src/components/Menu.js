ca580d768fefdbc3db1e65bd53e62839957db626
Nicolas Cragno <nicolaslautaro.cragno@gmail.com>
2025-01-30 11:57:15 -0300

    menu deplegable

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,36 +4,30 @@ import { Container, Row, Col } from 'react-bootstrap';
 import optionLogo from '../assets/statics/repairLogo.png';
 
 export default function Menu() {
+    const desplegarMenu = () => {
+        document.getElementById('menuPrincipal').style.width="20%";
+        document.getElementById('menuPrincipal').style.transition="1s";
+        setTimeout(() => {
+            document.getElementById('opcionOculta').style.display="flex";
+            document.getElementById('opcionOculta').style.transition="2s";
+        }, 400);
+    }
+
+    const ocultarMenu = () => {
+        document.getElementById('menuPrincipal').style.width="5%";
+        document.getElementById('menuPrincipal').style.transition="1s";
+        document.getElementById('opcionOculta').style.display="none";
+    }
+
   return (
-    <div className='menuPrincipal centrado'>
+    <div className='menuPrincipal centrado' id='menuPrincipal' onMouseOver={desplegarMenu} onMouseLeave={ocultarMenu}>
         <Container>
             <Row>
                 <Col xl={12} className='centrado'>
-                    <img src={optionLogo} className='bigOptionLogo' alt=''></img>
-                    <h4 className='menuOption centrado'>
+                    <img src={optionLogo} className='bigOptionLogo' alt='' id='opcionActiva'></img>
+                    <h4 className='menuOption centrado' id='opcionOculta'>
                         <img src={optionLogo} className='smallOptionLogo' alt=''></img>
                         NUEVA CARGA
-                    </h4>
-                </Col>
-                <Col xl={12} className='centrado'>
-                    <img src={optionLogo} className='bigOptionLogo' alt=''></img>
-                    <h4 className='menuOption centrado'>
-                        <img src={optionLogo} className='smallOptionLogo' alt=''></img>
-                        VIAJES
-                    </h4>
-                </Col>
-                <Col xl={12} className='centrado'>
-                    <img src={optionLogo} className='bigOptionLogo' alt=''></img>
-                    <h4 className='menuOption centrado'>
-                        <img src={optionLogo} className='smallOptionLogo' alt=''></img>
-                        CHOFERES
-                    </h4>
-                </Col>
-                <Col xl={12} className='centrado'>
-                    <img src={optionLogo} className='bigOptionLogo' alt=''></img>
-                    <h4 className='menuOption centrado'>
-                        <img src={optionLogo} className='smallOptionLogo' alt=''></img>
-                        COMPARATIVAS
                     </h4>
                 </Col>
             </Row>
